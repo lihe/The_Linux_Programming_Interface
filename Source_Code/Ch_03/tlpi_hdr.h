8032f34112caa8b70ac86f33e8a71dcb55f5b682
lihe <lihellowd@outlook.com>
2021-02-21 15:12:56 +0800

    Source_Code

---
+++ Source_Code/Ch_03/tlpi_hdr.h
@@ -0,0 +1,16 @@
+
+#ifndef TLPI_HDR_H
+#define TLPI_HDR_H  
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+typedef enum { FALSE, TRUE } Boolean;
+
+#define min(m, n) ((m) < (n) ? (m) : (n))
+#define max(m, n) ((m) > (n) ? (m) : (n))
+
+#endif
